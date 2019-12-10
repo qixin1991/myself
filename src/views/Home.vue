3609e18a70ee64c436ef9c32c28fedc989eff466
无忌 <me@qixin.io>
2019-12-10 16:29:57 +0800

    移动端简单适配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,10 +37,10 @@
       <a-timeline mode="alternate">
         <a-timeline-item>
           <span>2016/03 ~ Now</span>
-          <div>
-            <span class="title-company">架构师 @ 家装E站</span>
+          <div class="right_item">
+            <span class="title-company right_title">架构师 @ 家装E站</span>
           </div>
-          <div>
+          <div class="job_contents">
             <ul>
               <li>负责面向全国近500家分站的分站管理系统开发</li>
               <li>Nodejs 技术栈在内部的推广</li>
@@ -51,7 +51,7 @@
         <a-timeline-item color="green">
           <span>2015/11 ~ 2016/03</span>
           <div>
-            <span class="title-company">CTO @ 天天一百网络科技</span>
+            <span class="title-company left_title">CTO @ 天天一百网络科技</span>
           </div>
           <div class="left-desc">
             <ul class="left-ul">
@@ -63,12 +63,14 @@
           </div>
         </a-timeline-item>
         <a-timeline-item>
-          <a-icon slot="dot" type="clock-circle-o" style="font-size: 16px;" />
+          <a-icon slot="dot"
+                  type="clock-circle-o"
+                  style="font-size: 16px;" />
           <span>2015/01 ~ 2015/11</span>
-          <div>
-            <span class="title-company">系统架构师 @ 天下互联苏州分公司</span>
+          <div class="right_item">
+            <span class="title-company right_title">系统架构师 @ 天下互联苏州分公司</span>
           </div>
-          <div>
+          <div class="job_contents">
             <ul>
               <li>社交App Backend开发</li>
               <li>运维</li>
@@ -78,7 +80,7 @@
         <a-timeline-item color="green">
           <span>2014/09 ~ 2015/01</span>
           <div>
-            <span class="title-company">Java高级工程师 @ 苏州用心创造</span>
+            <span class="title-company left_title">Java高级工程师 @ 苏州用心创造</span>
           </div>
           <div class="left-desc">
             <ul class="left-ul">
@@ -88,20 +90,22 @@
         </a-timeline-item>
         <a-timeline-item>
           <span>2014/07 ~ 2014/09</span>
-          <div>
-            <span class="title-company">Java高级工程师 @ 苏州瑞鹏</span>
+          <div class="right_item">
+            <span class="title-company right_title">Java高级工程师 @ 苏州瑞鹏</span>
           </div>
-          <div>
+          <div class="job_contents">
             <ul>
               <li>外婆家 - 微信公众号</li>
             </ul>
           </div>
         </a-timeline-item>
         <a-timeline-item>
-          <a-icon slot="dot" type="clock-circle-o" style="font-size: 16px;" />
+          <a-icon slot="dot"
+                  type="clock-circle-o"
+                  style="font-size: 16px;" />
           <span>2013/11 ~ 2014/07</span>
           <div>
-            <span class="title-company">项目经理 @ 苏州梦图数码</span>
+            <span class="title-company left_title">项目经理 @ 苏州梦图数码</span>
           </div>
           <div class="left-desc">
             <ul class="left-ul">
@@ -112,8 +116,8 @@
         </a-timeline-item>
         <a-timeline-item color="green">
           <span>2011/08 ~ 2013/11</span>
-          <div>
-            <span class="title-company">Java开发工程师 @ 南京斯帝普科技有限公司</span>
+          <div class="right_item">
+            <span class="title-company right_title">Java开发工程师 @ 南京斯帝普科技有限公司</span>
           </div>
           <div>
             <ul>
@@ -141,78 +145,97 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-  .left-desc {
-    display: flex;
+.job_contents {
+  width: 85%;
+  & ul {
+    padding-left: 1rem;
   }
+}
 
-  .left-ul {
-    display: flex;
+.left-desc {
+  display: flex;
+}
+
+.left-ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  align-items: flex-end;
+  flex-direction: column;
+  padding-left: 1.2rem;
+  width: 100%;
+  li {
     flex-wrap: wrap;
-    justify-content: flex-end;
-    align-items: flex-end;
-    flex-direction: column;
-    width: 100%;
-    li {
-      flex-wrap: wrap;
-    }
   }
+}
 
-  .head {
-    display: flex;
-    justify-content: space-around;
-    flex-grow: 1;
-    width: 100%;
-    margin-top: 12px;
-    & .about-me {
-      color: #1790FF;
-      font-weight: bold;
-      font-size: 2em;
-    }
-    & .contact-me {
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
-    }
-  }
-
-  .title-company {
-    font-size: 16px;
+.head {
+  display: flex;
+  justify-content: space-around;
+  flex-grow: 1;
+  width: 100%;
+  margin-top: 12px;
+  & .about-me {
+    color: #1790ff;
     font-weight: bold;
+    font-size: 2em;
   }
-
-  .timeline {
-    margin: 2% auto;
+  & .contact-me {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
   }
+}
 
-  .experience {
+.title-company {
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.left_title {
+  margin-left: 1.2rem;
+}
+
+.right_title {
+  margin-right: 1.2rem;
+}
+
+.right_item {
+  width: 85%;
+}
+
+.timeline {
+  margin: 2% auto;
+}
+
+.experience {
+  font-size: 1em;
+  font-weight: bold;
+  color: #1790ff;
+  display: flex;
+  justify-content: center;
+}
+
+.tech-main {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & .title {
     font-size: 1em;
     font-weight: bold;
-    color: #1790FF;
-    display: flex;
-    justify-content: center;
+    color: #1790ff;
+    margin: 0 24px;
   }
-
-  .tech-main {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    & .title {
-      font-size: 1em;
-      font-weight: bold;
-      color: #1790FF;
-      margin: 0 24px;
-    }
-    & .ul-container {
-      margin: 12px 0;
-      padding-inline-start: 0;
-      & .li-title {
-        display: block;
-        float: left;
-        margin: 0 6px;
-        font-size: medium;
-      }
+  & .ul-container {
+    margin: 12px 0;
+    padding-inline-start: 0;
+    & .li-title {
+      display: block;
+      float: left;
+      margin: 0 6px;
+      font-size: medium;
     }
   }
+}
 </style>
