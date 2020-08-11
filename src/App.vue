2328768655839b0fd46b05e23e2485ee0fb64eb2
无忌 <me@qixin.io>
2020-08-11 09:03:35 +0800

    简单支持暗黑模式

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,15 @@
 </template>
 
 <style lang="scss">
+@media (prefers-color-scheme: dark) {
+  html {
+    filter: invert(1) hue-rotate(180deg);
+  }
+  html img {
+      filter: invert(1) hue-rotate(180deg);
+  }
+}
+html {
+  transition: color 300ms, background-color 300ms;
+}
 </style>
